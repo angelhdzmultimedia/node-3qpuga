29d6cba80ea25642059ea04f0ec66266c66dedc7
angelhdzmultimedia <angel.hernandez.12@live.com>
2023-01-15 05:16:42 -0400

    1 file modified, 1 deleted

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,15 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
-if (import.meta.env.PROD) {
-  async function bootstrap() {
-    const app = await NestFactory.create(AppModule)
-    app.enableCors({
-      origin: 'http://localhost:9000',
-    })
-    await app.listen(5000)
-  }
-  bootstrap()
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: 'http://localhost:9000',
+  })
+  await app.listen(5000)
 }
-
-export const viteNodeApp = NestFactory.create(AppModule)
+bootstrap()
